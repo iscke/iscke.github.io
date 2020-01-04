675052335d1084994050ac284d9c98686d0baca3
whales <jumbowhales@gmail.com>
2020-01-05 06:23:27 +1030

    Add roleparser test

---
+++ roleparser.js
@@ -0,0 +1,14 @@
+
+function parse() {
+    const roleString = document.getElementById('roleinput').value;
+    const {role, problems} = parseRole(roleString);
+
+    const detailsText = `<table><tr><td style="text-align:center;"><img width="75" height="75" src="https://play.pokemonshowdown.com/${role.image || 'fx/mafia-villager'}.png"></td><td style="text-align:left;width:100%"><ul>${role.memo.map(m => `<li>${m}</li>`).join('')}</ul></td></tr></table>`;
+    document.getElementById('details').innerHTML = detailsText;
+
+    const problemsText = `<ul>${problems.map(m => `<li>${m}</li>`).join('')}</ul>`;
+    document.getElementById('problems').innerHTML = problemsText;
+
+    document.getElementById('name').innerHTML = `Your role is: ${role.name}`;
+}
+

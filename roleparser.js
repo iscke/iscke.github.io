7030000f07568c8e09400b88df0aa0954cc76223
whales <jumbowhales@gmail.com>
2020-01-10 15:20:47 +1030

    Updates
    Use IDs
    Take out useless id prop
    Add GI subdecks (thanks claire!)
    
    Closes #3

--- roleparser.js
+++ roleparser.js
@@ -3,7 +3,7 @@ function parse() {
     const roleString = document.getElementById('roleinput').value;
     const {role, problems, ignored} = parseRole(roleString);
 
-    const detailsText = `<table><tr><td style="text-align:center;"><img width="75" height="75" src="https://play.pokemonshowdown.com/${role.image || 'fx/mafia-villager'}.png"></td><td style="text-align:left;width:100%"><ul>${role.memo.map(m => `<li>${m}</li>`).join('')}</ul></td></tr></table>`;
+    const detailsText = `<table><tr><td style="text-align:center;"><img width="75" height="75" src="https://play.pokemonshowdown.com/fx/mafia-${role.image || 'villager'}.png"></td><td style="text-align:left;width:100%"><ul>${role.memo.map(m => `<li>${m}</li>`).join('')}</ul></td></tr></table>`;
     document.getElementById('details').innerHTML = detailsText;
 
     const problemsText = `<ul>${problems.map(m => `<li>${m}</li>`).join('')}</ul>`;
